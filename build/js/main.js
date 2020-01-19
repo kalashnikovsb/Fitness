213bab9484913b8fb8899d09887648dffa16113d
kalashnikovsb <kalashnikov.sk8@gmail.com>
2020-01-20 01:26:59 +0700

    Добавил маску для ввода телефона и сделал ретинизацию

---
+++ build/js/main.js
@@ -0,0 +1,9 @@
+var telNumber = document.querySelector('.main-form__input[name="customer-phone"]');
+
+var telOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+
+if (telNumber) {
+  var popupMask = IMask(telNumber, telOptions);
+}
